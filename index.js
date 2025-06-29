f41c10fa28b0dfb73e741146cd4f432eb839e55c
aiyazalam-hub <aiyazalam443@gmail.com>
2025-06-29 07:53:27 +0530

    content section complete

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ function closesidebar() {
 openbtn.addEventListener("click", opensidebar);
 closebtn.addEventListener("click", closesidebar);
 
+
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+  const question = item.querySelector('.faq-question');
+
+  question.addEventListener('click', () => {
+    item.classList.toggle('active');
+
+    faqItems.forEach(otherItem => {
+      if (otherItem !== item) {
+        otherItem.classList.remove('active');
+      }
+    });
+  });
+});
+
 // splide initialize
 var splide = new Splide(".splide", {
   perPage: 4,
@@ -46,4 +63,6 @@ var splide = new Splide( '.splide1', {
 } );
 
 splide.mount();
+
+
 
